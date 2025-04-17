8dae369745b4761d495fb1f62eb57680acd497fa
Rahul <rahulraazrs123@gmail.com>
2025-04-17 23:01:42 +0545

    First Commit Done...

---
+++ app/src/main/java/com/example/firstapp/Arraylist.kt
@@ -0,0 +1,29 @@
+package com.example.firstapp
+
+
+
+fun main(args:Array<String>) {
+    var aqe = arrayOf(1,2,3)
+    println(aqe)
+    println("The first element of age is "+ aqe[0])
+    println("The second element of age is "+ aqe[1])
+
+    println( "third element of age is "+ aqe[2])
+    println("*************************************")
+    var name = arrayOf("ram","shyam" , "Hari")
+
+    name[1] = "sandis"
+    println("The first element of name is "+ name[0])
+
+    println("The second element of name is "+ name[1])
+    println("The third element of name is "+ name[2])
+    println(name.size)
+
+
+    //we con add value only after our orray is initialize
+    var age1= ArrayList<Int>()
+    age1.add(1)
+    age1.add(5)
+
+    var age2 = arrayListOf()
+}
---
+++ app/src/main/java/com/example/firstapp/Fundamentals.kt
@@ -0,0 +1,39 @@
+package com.example.firstapp
+
+fun main(args: Array<String>) {
+    println("Hello World")
+
+
+// mutable
+//we can re assign values to mutable variables
+    var age = 20;
+    age = 25
+// immutable
+//we can't re assign values to immutable variables
+    var roll = 15
+    roll = 10
+
+    //Datatypes
+
+    var a : Boolean = true
+    var b : Boolean = true
+    var c : Boolean = true
+    var d : Boolean = true
+    var e : Boolean = true
+    var f : Boolean = true
+    var i : Boolean = true
+    var h : Boolean = true
+
+
+    println(a)
+    println(b)
+    println(c)
+    println(d)
+    println(e)
+    println(f)
+    println(h)
+    println(i)
+    println(h)
+
+
+}

7e8f90ee4a58b603aa7645f6315061ea24aa18a1
Rahul <rahulraazrs123@gmail.com>
2025-04-25 19:04:34 +0545

    "profile Icon Added"

--- app/src/main/java/com/example/firstapp/ui/theme/Color.kt
+++ app/src/main/java/com/example/firstapp/ui/theme/Color.kt
@@ -3,7 +3,6 @@ package com.example.firstapp.ui.theme
 import androidx.compose.ui.graphics.Color
 
 val Purple80 = Color(0xFFD0BCFF)
-val greenone = Color(0xFF009688)
 
 val PurpleGrey80 = Color(0xFFCCC2DC)
 val Pink80 = Color(0xFFEFB8C8)
